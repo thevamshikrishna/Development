3dae1a0dc1dca9081b23f936bdcc91eea8f609b2
vamshinallavalli <vamshi.nallavalli@atyeti.com>
2025-09-29 15:31:16 +0530

    Added Experiments project

---
+++ src/main/java/com/atyeti/Experiments/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.atyeti.Experiments;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExperimentsApplication.class);
+	}
+
+}
---
+++ src/test/java/com/atyeti/Experiments/ExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atyeti.Experiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
